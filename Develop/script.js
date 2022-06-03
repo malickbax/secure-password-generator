add3af24b387bfe318e3176a4cdf16b81b19d47b
malickbax <malickbax@gmail.com>
2022-06-03 12:57:19 -0500

    added while element that allows the prompt about characters to be repeated after user responds no to all prompts

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@
   // 1. When user clicks the button to generate a password, 
   // user is promted to choose the length of password between 8 and 128 characters
 
-var passwordLength = prompt("How many characters do you want your password to be? Enter a number between 8 and 128")
+var passwordLength = number = prompt("How many characters do you want your password to be? Enter a number between 8 and 128")
   if (passwordLength < 8 || passwordLength > 128){
     alert("Your password must be between 8 and 128 characters long. Please enter a number in that range")
 }
@@ -20,14 +20,13 @@ var passwordLength = prompt("How many characters do you want your password to be
 
 // 3. At least one character type should be selected means: If user answers no to lowercase, 
 // uppercase, numeric, and special characters, then system automatically alerts user to choose at least one type to proceed. 
-  if (spec != true && num != true && lowercase != true && uppercase != true){
-    alert("Your password must include at least one character type. Please select at least one: upper case, lower case, number, or special character")
+  while (spec != true && num != true && lowercase != true && uppercase != true){
+    alert("Your password must include at least one character type. Please select at least one: upper case, lower case, number, or special character");
+    var uppercase = confirm ("Do you want UPPER CASE included in your password?")
+    var lowercase = confirm ("Do you want lower case included in your password?")
+    var num = confirm ("Do you want numbers included in your password?")
+    var spec = confirm ("Do you want special characters included in your password?")
 }
-
-
-
-
-
 
 // 4. After all prompts are answered, a password w/ selected criteria is generated 
 // in an alert or written to the page
@@ -35,6 +34,14 @@ var passwordLength = prompt("How many characters do you want your password to be
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
