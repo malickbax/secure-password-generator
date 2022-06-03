9f6e897aaa63353cb4c63b481f4c608239baf0d6
malickbax <malickbax@gmail.com>
2022-06-03 12:34:25 -0500

    added steps 1-3 of the process and initial functions associated with password character types selection

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,33 @@
 // Assignment code here
 
-// 1. When user clicks the button to generate a password, 
-// user is promted to choose the length of password between 8 and 128 characters
-var generatePassword = function () {
-  var length = "";
+  // 1. When user clicks the button to generate a password, 
+  // user is promted to choose the length of password between 8 and 128 characters
 
-  while (length === "" || length === null) {
-    length = prompt("How many characters do you want your password to be? Type a number between 8 and 128");
-  }
-  console.log("Your password will be " + length + " characters long");
-};
+var passwordLength = prompt("How many characters do you want your password to be? Enter a number between 8 and 128")
+  if (passwordLength < 8 || passwordLength > 128){
+    alert("Your password must be between 8 and 128 characters long. Please enter a number in that range")
+}
 
-// Based on the number typed by the user, the system generates 
-// an array with an index corresponding to the number of characters reauested
+// 2. After that, System prompts user to choose whether they want to include uppercase, 
+// lowercase, number, and/or special characters
 
-
-// 2. After that, System prompts user to choose whether they want to include lowercase, 
-// uppercase, numeric, and/or special characters
-
+  if (passwordLength >=8 && passwordLength <= 128){
+    var uppercase = confirm ("Do you want UPPER CASE included in your password?")
+    var lowercase = confirm ("Do you want lower case included in your password?")
+    var num = confirm ("Do you want numbers included in your password?")
+    var spec = confirm ("Do you want special characters included in your password?")
+  } 
 
 // 3. At least one character type should be selected means: If user answers no to lowercase, 
-// uppercase and numeric, then system automatically chooses yes on the special characters question.
+// uppercase, numeric, and special characters, then system automatically alerts user to choose at least one type to proceed. 
+  if (spec != true && num != true && lowercase != true && uppercase != true){
+    alert("Your password must include at least one character type. Please select at least one: upper case, lower case, number, or special character")
+}
+
+
+
+
+
 
 // 4. After all prompts are answered, a password w/ selected criteria is generated 
 // in an alert or written to the page
@@ -40,3 +47,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+// var generatePassword = function () {
+//   var length = "";
+//   if (length < 8 || length > 128){
+//     alert("Length must be 8-128 characters")
+// }
+  
+//   console.log("Your password will be " + length + " characters long");
+// };
+
+// Based on the number typed by the user, the system generates 
+// an array with an index corresponding to the number of characters requested
