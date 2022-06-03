8dc16107957a3351710a1cbc1d80dbff4793495f
malickbax <malickbax@gmail.com>
2022-06-03 10:51:10 -0500

    created function for password length selection

--- Develop/script.js
+++ Develop/script.js
@@ -2,27 +2,28 @@
 
 // 1. When user clicks the button to generate a password, 
 // user is promted to choose the length of password between 8 and 128 characters
+var generatePassword = function () {
+  var length = "";
+
+  while (length === "" || length === null) {
+    length = prompt("How many characters do you want your password to be? Type a number between 8 and 128");
+  }
+  console.log("Your password will be " + length + " characters long");
+};
+
+// Based on the number typed by the user, the system generates 
+// an array with an index corresponding to the number of characters reauested
+
 
 // 2. After that, System prompts user to choose whether they want to include lowercase, 
 // uppercase, numeric, and/or special characters
+
 
 // 3. At least one character type should be selected means: If user answers no to lowercase, 
 // uppercase and numeric, then system automatically chooses yes on the special characters question.
 
 // 4. After all prompts are answered, a password w/ selected criteria is generated 
 // in an alert or written to the page
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // Get references to the #generate element
