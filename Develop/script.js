488a2343868dd070fc4a74eee016160a7e1a6084
malickbax <malickbax@gmail.com>
2022-06-03 07:35:56 -0500

    inserted pseudo codes in JS file to initiate logic behind password generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,28 @@
 // Assignment code here
+// WHEN user clicks the button to generate a password,
+// THEN user is presented with a series of prompts for password criteria
+// WHEN prompted for password criteria, user selects which criteria to include in the password
+
+
+// WHEN prompted for the length of the password
+// THEN user chooses a length of between 8 and 128 characters
+
+
+
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+
+
 
 
 // Get references to the #generate element
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # secure-password-generator
+- This is an application that employees can use to generate a random and secure password based on criteria they’ve selected. 
+- This app runs in the browser, and features dynamically updated HTML and CSS powered by JavaScript. 
+- The application has a user responsive user interface, adapting to multiple screen sizes. 
